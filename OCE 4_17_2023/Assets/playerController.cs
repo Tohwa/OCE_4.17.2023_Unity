e7d435ebfbdb53d9824f3c264db4483213e5ec13
Sascha <Tohwa.gaming@gmail.com>
2023-04-17 12:48:17 +0200

    added some components to player

--- OCE 4_17_2023/Assets/playerController.cs
+++ OCE 4_17_2023/Assets/playerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody2D m_rb;;
+    private Rigidbody2D m_rb;
+
+    [SerializeField]
+    private GameObject m_SisterGameObject;
 
     private Vector2 movement;
 
@@ -16,13 +19,17 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+
+        m_SisterGameObject = GameObject.FindGameObjectWithTag("bla");
         m_rb = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-        m_rb.AddForce(movement = movementSpeed + Time.deltaTime);
+        m_rb.AddForce(movement);
     }
 
     public void Move(InputAction.CallbackContext ctx)
